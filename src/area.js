907c034e24bcea52f904a450b9a2d537006e1789
demmanuel2004 <55405293+demmanuel2004@users.noreply.github.com>
2019-10-22 16:15:47 -0400

    Update area.js

--- src/area.js
+++ src/area.js
@@ -1,8 +1,16 @@
 const readlineSync = require("readline-sync");
 
-const IN_TO_MM
+const IN_TO_MM = 25.4;
 
-const lengthInInches = (Number readlineSync.question =("Width: "));
-const widthInInches; = (Number readlinesync.question =("Length :"));
+let widthInInches = Number(readlineSync.question("Width: "));
+let lengthInInches = Number(readlineSync.question("Length: "));
 
-console.log("A(n)" + lengthInInches + " by" + widthInInches  "sheet of paper is "
+let width_mm = widthInInches * IN_TO_MM;
+let length_mm = lengthInInches * IN_TO_MM;
+let area = width_mm * length_mm;
+
+width_in = String(width_in);
+length_in = String(length_in);
+
+console.log("\nA(n) " + widthInInches + "-by-" + lengthInInches + "-inch sheet of paper has an area of " + area + " square millimeter(s)." );
+
