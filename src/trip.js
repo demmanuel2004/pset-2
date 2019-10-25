62382791acd5961c146ba9ce186220ff13b87262
demmanuel2004 <55405293+demmanuel2004@users.noreply.github.com>
2019-10-25 11:54:24 -0400

    Update trip.js

--- src/trip.js
+++ src/trip.js
@@ -1,10 +1,19 @@
 const readlineSync = require("readline-sync");
 
+
+
 const students = Number(readlineSync.question("Students: "));
+
 const teachers = Number(readlineSync.question("Teachers: "));
+
 const bus_capacity = Number(readlineSync.question("Bus Capacity: "));
 
-let bus_total = (students+teachers)/bus_capacity
-let lastBusPeople = bus_total % bus_capacity;
 
-console.log(+ bus_total + "\nbus(es) is (are) needed, with" + lastBusPeople +  " passenger(s) on the last bus.\n");
+
+let bus_total = Math.trunc((students+teachers)/bus_capacity) + 1;
+
+let lastBusPeople = (students + teachers) % bus_capacity;
+
+
+
+console.log(+ bus_total + " bus(es) is (are) needed, with " + lastBusPeople +  " passenger(s) on the last bus.\n");
