28ccd0580c6ec87ba5f44d4ce40d951bb2776783
Daniel Emmanuel <[DEmmanuel@ucvts.tec.nj.us]>
2019-10-26 17:28:25 -0400

    finished salary

--- src/salary.js
+++ src/salary.js
@@ -7,13 +7,16 @@ const FED_INCOME_TAX = 0.157;
 const STATE_INCOME_TAX = 0.0447;
 const SOC_SEC_TAX = 0.062;
 const MED_TAX = 0.0145;
+const BI_MONTHLY_PAY = 24;
 
 
-let payCheck = (ANNUAL_SALARY / 2);
+let payCheck = ANNUAL_SALARY;
+let preTaxSalary = payCheck - (payCheck * PRETAX);
+let totalDeductionPercentage = FED_INCOME_TAX + STATE_INCOME_TAX + SOC_SEC_TAX + MED_TAX;
 
-let totalDeductionPercentage = PRETAX + FED_INCOME_TAX + STATE_INCOME_TAX + SOC_SEC_TAX + MED_TAX;
+let annualPayCheck = preTaxSalary - (preTaxSalary * totalDeductionPercentage)
+let netPayCheck = annualPayCheck / BI_MONTHLY_PAY;
 
+netPayCheck = netPayCheck.toLocaleString("en", { style: "currency", currency: "USD" })
 
-takeHomeCheck = //to be finished
-
-console.log("\nYour take-home pay each check will be " + takeHomeCheck + ".");
+console.log("\nYour take-home pay each check will be " + netPayCheck + ".");
