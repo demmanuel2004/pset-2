f748e9e55aa93b6b19288a844c7286a9fb3f7f21
Daniel Emmanuel <[DEmmanuel@ucvts.tec.nj.us]>
2019-10-26 17:30:35 -0400

    fixed formatting

--- src/trip.js
+++ src/trip.js
@@ -1,19 +1,11 @@
 const readlineSync = require("readline-sync");
 
 
-
 const STUDENTS = Number(readlineSync.question("Students: "));
-
 const TEACHERS = Number(readlineSync.question("Teachers: "));
-
 const BUS_CAPACITY = Number(readlineSync.question("Bus Capacity: "));
 
-
-
 let bus_total = Math.trunc((STUDENTS+TEACHERS)/BUS_CAPACITY) + 1;
-
 let lastBusPeople = (STUDENTS + TEACHERS) % BUS_CAPACITY;
-
-
 
 console.log(+ bus_total + " bus(es) is (are) needed, with " + lastBusPeople +  " passenger(s) on the last bus.\n");
